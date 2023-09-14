4fad59948bdc043499a87946a4cd5f84e1ee1666
jeremy <jeremyfelix41@gmail.com>
2023-09-14 17:03:29 +0200

    Installation du CORS

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@ console.log('Hello world');
 
 import express from "express"
 import "dotenv/config"
+import cors from "cors"
 
 const app = express()
+app.use(cors());
 const port = parseInt(process.env.PORT as string)
-
 
 
 import { DataTypes, Sequelize } from "sequelize"
@@ -15,14 +16,63 @@ const sequelize = new Sequelize({
   storage: "./db.sqlite",
 })
 
+const RecipesBdd = sequelize.define("RecipesBdd", {
+  nom: {
+    type: DataTypes.STRING,
+  },
+  duree: {
+    type: DataTypes.NUMBER,
+  },
+  note: {
+    type: DataTypes.NUMBER,
+  },
+  url: {
+    type: DataTypes.STRING,
+  },
+}, {
+  timestamps: false,
+})
+
+sequelize
+  .sync({ force: true })
+  .then(() => {
+    console.log('La synchronisation a réussi.');
+    RecipesBdd.create({
+    })
+    .then((recipe) => {
+      console.log("recipe", recipe)
+      RecipesBdd.findAll().then((recipes) => {
+        console.log("recipes", recipes)
+      })
+    })
+  })
+  .catch(error => {
+    console.error('Erreur de synchronisation:', error);
+  });
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/toto/', (req, res) => {
-    res.send('Toto')
-  })
+app.post('/add_recipe/:name/:duree/:note/', (req, res) => {
+    let recipeName = req.params.name;
+    let recipeDuration = req.params.duree;
+    let recipeNote = req.params.note;
+    //let recipeUrl = req.params.url;
+
+    console.log("recipeNote = " + recipeNote);
+
+    RecipesBdd.create({
+      nom: recipeName as string,
+      duree: parseInt(recipeDuration) as number,
+      note: parseInt(recipeNote) as number,
+      /*url: recipeUrl as string*/
+    })
+
+    res.send('La recette a été rajoutée avec succès !')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
